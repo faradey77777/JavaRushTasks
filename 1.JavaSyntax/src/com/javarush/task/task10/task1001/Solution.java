9858b2034e0dda77e732570ac40f65539b01c702
Faradey77777 <jaroshenko.dmitriy@gmail.com>
2019-04-17 17:30:07 +0300

    first 5 tasks

--- 1.JavaSyntax/src/com/javarush/task/task10/task1001/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1001/Solution.java
@@ -9,8 +9,8 @@ public class Solution {
         int a = 0;
         int b = (byte) a + 46;
         byte c = (byte) (a * b);
-        double f = (char) 1234.15;
-        long d = (short) (a + f / c + b);
+        double f = (float) 1234.15;
+        long d = (int) (a + f / c + b);
         System.out.println(d);
     }
 }
