d788703c28f2315ea6a18ccfa98d9ca69c276f64
Faradey77777 <jaroshenko.dmitriy@gmail.com>
2019-04-20 09:48:27 +0300

    Start level 1(Core)

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task10.task1013;
+
+/* 
+Конструкторы класса Human
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static class Human {
+        private String name;
+        private String surname;
+        private int age;
+        private String address;
+        private int birth;
+        private int tel;
+    }
+}
