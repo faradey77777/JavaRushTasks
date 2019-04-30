51cb799e0a65dba34a948b46f896171669afb3b5
Faradey77777 <jaroshenko.dmitriy@gmail.com>
2019-04-30 12:30:57 +0300

    level 3 Core clear

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        key = reader.readLine();
+        switch (key){
+            case "user":
+                Person person1 = new Person.User();
+                doWork(person1);
+                break;
+            case "loser":
+                Person person2 = new Person.Loser();
+                doWork(person2);
+                break;
+            case "proger":
+                Person person3 = new Person.Proger();
+                doWork(person3);
+                break;
+            case "coder":
+                Person person4 = new Person.Coder();
+                doWork(person4);
+                break;
+            default:
+              reader.close();
+                break;
+
+    }
+
+    }
+
+    public static void doWork(Person person) {
+        if(person instanceof Person.User){
+        ((Person.User) person).live();
+        }
+        if(person instanceof Person.Loser){
+            ((Person.Loser) person).doNothing();
+        }
+        if(person instanceof Person.Coder){
+            ((Person.Coder) person).writeCode();
+        }
+        if(person instanceof Person.Proger){
+            ((Person.Proger) person).enjoy();
+        }
+
+    }
+}
